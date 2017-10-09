75fe508ee1f2cc7669e4fa8cce4969f3cc8a5cdc
Ustim93 <Yustim@ukr.net>
2017-10-09 21:25:09 +0300

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -4,6 +4,15 @@
 int main()
 {
 int i,j,k;
-double a[N][N], b[N][N], c[N][N];
+int a[N][N], b[N][N], c[N][N];
 
-  for(
+  for (i=0; i<N; i++)
+    for (j=0; j<N; j++)
+      a[i][j]=b[i][j]=i*j;
+
+  for (i=0; i<N; i++){
+    for (j=0; j<N; j++){
+      c[i][j]=0;
+      for(k=0; k<N; k++) c[i][j]+=a[i][k]*b[k][j];
+    }
+  }
