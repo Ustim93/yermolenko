0614f0380729684ef90760c1d540a57198d33306
Ustim93 <Yustim@ukr.net>
2017-10-30 22:35:36 +0300

    Update vector-class (Grygoriev).cpp

--- vector-class (Grygoriev).cpp
+++ vector-class (Grygoriev).cpp
@@ -1,56 +1,74 @@
+// ConsoleApplication7.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
-class vector
+class Vector
 {
 private:
-int *pointer; //вказівник на масив, що міститься в стеку
-int size;//розмір (к-сть елементів) стеку
-int capacity; //сколько элементов мы можем хранить в выделенной памяти на куче.
+	int *pointer; //вказівник на масив, що міститься в стеку
+	int size;//розмір (к-сть елементів) стеку
+	int capacity; //сколько элементов мы можем хранить в выделенной памяти на куче.
 public:
-vector(int capacity) 
-{
-	pointer = new int[capacity];
-	this ->capacity = capacity;
-	tsis ->size = 0;
+	vector(int capacity)
+	{
+		pointer = new int[capacity];
+		this->capacity = capacity;
+		tsis->size = 0;
+	}
+
+	int size
+	{ return size; }
+
+		void push_back(int element)
+	{
+		size++;
+		if (size <= capacity)
+			pointer[size - 1] = element;
+		if (size > capacity)
+		{
+			int *temp = new int[size];
+			for (int i = 0; i<(size - 1); i++)
+			{
+				temp[i] = pointer[i];
+			}
+
+			temp[size - 1] = element;
+
+			delete[] pointer;
+
+			pointer = new int[size];
+
+			for (int i = 0; i<size; i++)
+			{
+				pointer[i] = temp[i];
+			}
+
+			delete[] temp;
+			capacity++;
+
+		}
+	}
+
+	int at(int i) { return pointer[i]; }
+
+
+
+	~vector() { delete[] pointer; }
 }
 
-int size
-{return size;}
-	
-void push_back (int element)
+int main()
 {
-	size++;
-	if(size <= capacity)
-		pointer[size - 1] = element;
-	if(size > capacity)
-	{
-		int *temp = new int[size];
-		for(int i = 0; i<(size - 1); i++)
-		{
-			temp[i] = pointer[i];
-		}
-		
-	        temp[size - 1] = element;
-		
-		delete [] pointer;
-		
-		pointer = new int[size];
-		
-		for(int i = 0; i<size; i++)
-		{
-			 pointer[i] = temp[i];
-		}
-		
-		delete [] temp;
-		capacity++;
-		
-	}
+	Vector object(10);
+
+	for (int j = 1; j = 5; j++) object.push_back(2 * j);
+
+	cout << object.size << endl;
+
+	for (int j = 1; j = 5; j++) cout<<object(j-1);
+
+    return 0;
 }
-	
-int at (int i) {return pointer[i];}
-					  
-	
-	
-~vector() {delete [] pointer;}
-}
+
