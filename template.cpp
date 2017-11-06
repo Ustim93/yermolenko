a85f2d29b8f094d96f3f04c29396dd55614f9148
Ustim93 <Yustim@ukr.net>
2017-11-07 02:58:00 +0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -8,52 +8,54 @@
 using namespace std;
 
 template <typename T>
-void sort(T *a, int i){
-int b[i], c[i];
+void sort(T *a, int i) {
+	T b[i];
+	bool c[i];
 
-int j,k,l,ind;
-	
-for(j = 0; j<i; j++)  c[j] = 1;
-	
-for(j = 0; j<i; j++){
-	k=0;
-	
-	while(!c[k]) k++;
-	ind = k;
-	b[j]=a[k]
-	
-	for(l=k; l<i;l++){
-		if(a[l]>b[j] && c[l] == 1){
-			b[j] = a[l];
-			ind = l; }
-			  }
-	
+	int j, k, l, ind;
+
+	for (j = 0; j<i; j++)  c[j] = 1;
+
+	for (j = 0; j<i; j++) {
+		k = 0;
+
+		while (!c[k]) k++;
+		ind = k;
+		b[j] = a[k];
+
+			for (l = k; l<i; l++) {
+				if (a[l]>b[j] && c[l] == 1) {
+					b[j] = a[l];
+					ind = l;
+				}
+			}
+
 		c[ind] = 0;
+	}
+
+	for (j = 0; j<i; j++) a[j] = b[j];
+
+	for (j = 0; j<i; j++) cout << a[j] << " ";
+
+	cout << endl;
 }
-		
-	for(j = 0; j<i; j++) a[j]=b[j];		
-	
-	for(j = 0; j<i; j++) cout<<a[j]<<" ";
-	
-	cout<<endl;
-}
-  
+
 
 
 int main()
 {
-int i[] = {3,1,7,-10,256};
-sort(i,5);
-	
-double d[] = {3.0, 2057.12537, -5685.5678, 0.0, 297.754};
-sort(d,5);
-	
-char t[] = "Grygoriev";
-sort(t,9);
-	
-bool b[] = {1,0,1,1,0}
-sort(b,9);
-	
-    return 0;
+	int i[] = { 3,1,7,-10,256 };
+	sort(i, 5);
+
+	double d[] = { 3.0, 2057.12537, -5685.5678, 0.0, 297.754 };
+	sort(d, 5);
+
+	char t[] = "Grygoriev";
+	sort(t, 9);
+
+	bool b[] = { 1,0,1,1,0 };
+	sort(b, 9);
+
+	return 0;
 }
 
