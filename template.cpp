c07bbfea576129b7ff287360137baef4f72adbb4
Ustim93 <Yustim@ukr.net>
2017-11-07 02:35:34 +0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -11,8 +11,30 @@ template <typename T>
 void sort(T *a, int i){
 int b[i], c[i];
 
-for(int j = 0; j<i; j++){
-  c[j] = 1;
+int j,k,l,ind;
+	
+for(j = 0; j<i; j++)  c[j] = 1;
+	
+for(j = 0; j<i; j++){
+	k=0;
+	
+	while(!c[k]) k++;
+	ind = k;
+	b[j]=a[k]
+	
+	for(l=k; l<i;l++){
+		if(a[l]>b[j] && c[l] == 1){
+			b[j] = a[l];
+			ind = l;
+}
+}
+	
+	c[ind] = 0;
+}
+		
+			
+	
+	
   
 
 
