9c1a75202510d1c3011edfb7668b6ed9b75d62fc
Ustim93 <Yustim@ukr.net>
2017-11-07 01:20:11 +0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+// ConsoleApplication4.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void sort(T *a, int i){
+int b[i], c[i];
+
+for(int j = 0; j<i; j++){
+  c[j] = 1;
+  
+
+
+int main()
+{
+
+	
+    return 0;
+}
+
