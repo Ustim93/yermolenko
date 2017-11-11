e89eb33dbb2572b036fd3c79cfdf4fb848c2e9ae
Ustim93 <Yustim@ukr.net>
2017-11-12 02:20:58 +0300

    Update and rename Vector-structure.cpp to array_of_structures.cpp

---
+++ Григор`єв/array_of_structures.cpp
@@ -0,0 +1,47 @@
+// ConsoleApplication20.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+#include <iostream>
+
+
+using namespace std;
+
+struct MyStruct {
+	char name[255];
+	int mark;
+	bool status;
+};
+
+void print(const MyStruct obj[], int n)
+{
+
+	fot(int j = 0; j<n; j++)
+		cout << "Name:\t" << obj[j].name << "\t Mark: \t" << obj[j].mark << "\t Status \t" << obj[j].status << endl;
+}
+
+int main()
+{
+
+	const int n = 3;
+
+	MyStruct student[n] = {};
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "\n Name:\t";
+		cin >> student[i].name;
+
+
+		cout << "\n Mark:\t";
+		cin >> student[i].mark;
+
+
+		cout << "\n Status:\t";
+		cin >> student[i].status;
+	}
+
+	return 0;
+}
