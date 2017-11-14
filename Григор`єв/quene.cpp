87ee26fb9202db3f4f9b24367bc5d89dac573e07
Ustim93 <Yustim@ukr.net>
2017-11-14 17:49:36 +0200

    Update quene.cpp

--- Григор`єв/quene.cpp
+++ Григор`єв/quene.cpp
@@ -8,5 +8,49 @@ int main ()
   quene<int*> num;
   quene<int*> num2;
   
+  int a,b;
+  
+  cin >> a;
+  
+  while(!a)
+  {
+    b = (a*2 + 7)/3;
+    num.push((a*2 + 7)/3);
+    num2.push((a*2 + 3)/7);
+    
+  }
+  
+  num.swap(num2);
+  
+  if(num.empty()) cout << "Sorry, there is some problem!"<<endl;
+  
+  while(!num.empty())
+    
+  {
+    cout <<num.front() <<"   ";
+    num.pop();
+  }
+   
+  cout<<endl;
+    
+  while(!num2.empty())
+    
+  {
+    cout <<num2.front() <<"  ";
+    num.pop();
+  }
+    
+     
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+  
   return 0;
 }
